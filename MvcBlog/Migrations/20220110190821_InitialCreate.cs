eaae9dff8c556f57e5634506a34356b50b2948b5
kssd486 <kssd486@gmail.com>
2022-01-11 00:19:23 +0500

    Добавьте файлы проекта.

---
+++ MvcBlog/Migrations/20220110190821_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcBlog.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    Author = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Post");
+        }
+    }
+}
---
+++ MvcBlog/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Заголовок")]
+        public string Title { get; set; }
+
+        [Display(Name = "Дата создания")]
+        [DataType(DataType.Date)]
+        public DateTime CreationDate { get; set; }
+
+        [Display(Name = "Автор")]
+        public string Author { get; set; }
+
+        [Display(Name = "Текст")]
+        public string Text { get; set; }
+    }
+}
